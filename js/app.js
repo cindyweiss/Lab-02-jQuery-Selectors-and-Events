075d434d1733a471e004cba7eb0bc78a4c59f15b
cindyweiss <weiss_cindy@yahoo.com>
2019-12-04 11:07:21 -0800

    adding the pictures to page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 'use strict';
+const picarr = [];
+
+function Pic(picObj) {
+  this.title = picObj.title;
+  this.description = picObj.description;
+  this.keyWord = picObj.keyWord;
+  this.horns = picObj.horns;
+  this.image_url = picObj.image_url;
+  picarr.push(this);
+};
+
+Pic.prototype.render = function () {
+  const picTemplate = $('#pic-template').html();
+  const $newSection = $('<section></section>');
+  $newSection.html(picTemplate);
+  $newSection.find('h2').text(this.title);
+  $newSection.find('img').attr('src', this.image_url);
+  $newSection.find('img').attr('alt', this.keyWord);
+  $newSection.find('p').text(this.description);
+  $('main').append($newSection);
+};
+
+$.get('./data/page-1.json', data => {
+  data.forEach(pic => {
+    new Pic(pic).render();
+  });
+});
